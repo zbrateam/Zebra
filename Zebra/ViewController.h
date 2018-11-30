70b79542b2f6d37f99debc4c10bca57d66d9783b
Wilson Styres <wstyres@gmail.com>
2018-11-30 12:56:53 -0500

    Let's try this again

---
+++ Zebra/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Zebra
+//
+//  Created by Wilson Styres on 11/30/18.
+//  Copyright © 2018 Wilson Styres. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Zebra/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  Zebra
+//
+//  Created by Wilson Styres on 11/30/18.
+//  Copyright © 2018 Wilson Styres. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
