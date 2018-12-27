4f4730f9943dce19d718856b29287c1f55d8413f
Wilson Styres <wstyres@gmail.com>
2018-12-27 15:38:10 -0500

    [Packages] Load a certain number of rows for larger repos and then load 100 at a time once needed

--- Zebra/Packages/Controllers/ZBPackageListTableViewController.m
+++ Zebra/Packages/Controllers/ZBPackageListTableViewController.m
@@ -13,6 +13,7 @@
     ZBDatabaseManager *databaseManager;
     NSArray *packages;
     int numberOfPackages;
+    BOOL needsExpansion;
 }
 @end
 
@@ -21,6 +22,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    needsExpansion = false;
     databaseManager = [[ZBDatabaseManager alloc] init];
     if (_repoID == 0) {
         packages = [databaseManager installedPackages];
@@ -33,23 +35,26 @@
 }
 
 - (void)loadNextPackages {
-    NSLog(@"Loading next packages! start %d, going %d", numberOfPackages, numberOfPackages + 100);
+    NSLog(@"[Zebra] Loading next packages! start %d, going %d", numberOfPackages, numberOfPackages + 100);
     NSArray *nextPackages = [databaseManager packagesFromRepo:_repoID numberOfPackages:100 startingAt:numberOfPackages];
     packages = [packages arrayByAddingObjectsFromArray:nextPackages];
     numberOfPackages = (int)packages.count;
-    NSLog(@"Done %d", numberOfPackages);
+    NSLog(@"[Zebra] Done %d", numberOfPackages);
     
-//    NSMutableArray *indexArray = [NSMutableArray new];
-//    for (int i = numberOfPackages - 100; i < numberOfPackages; i++) {
-//        NSIndexPath *indexPath = [NSIndexPath indexPathForRow:i inSection:0];
-//        [indexArray addObject:indexPath];
-//    }
-//
-//    dispatch_async(dispatch_get_main_queue(), ^{
-//        [self.tableView beginUpdates];
-//        [self.tableView insertRowsAtIndexPaths:indexArray withRowAnimation:UITableViewRowAnimationNone];
-//        [self.tableView endUpdates];
-//    });
+    if (numberOfPackages > [self.tableView numberOfRowsInSection:0]) {
+        needsExpansion = true;
+        NSMutableArray *indexArray = [NSMutableArray new];
+        for (int i = numberOfPackages - 100; i < numberOfPackages; i++) {
+            NSIndexPath *indexPath = [NSIndexPath indexPathForRow:i inSection:0];
+            [indexArray addObject:indexPath];
+        }
+        
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [self.tableView beginUpdates];
+            [self.tableView insertRowsAtIndexPaths:indexArray withRowAnimation:UITableViewRowAnimationNone];
+            [self.tableView endUpdates];
+        });
+    }
 }
 
 #pragma mark - Table view data source
@@ -59,7 +64,18 @@
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return [databaseManager numberOfPackagesInRepo:_repoID];
+    if (needsExpansion) {
+        return numberOfPackages;
+    }
+    else if ([databaseManager numberOfPackagesInRepo:_repoID] > 1000) {
+        return 1000;
+    }
+    else if ([databaseManager numberOfPackagesInRepo:_repoID] > 500) {
+        return 500;
+    }
+    else {
+        return [databaseManager numberOfPackagesInRepo:_repoID];
+    }
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
