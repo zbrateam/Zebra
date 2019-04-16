5ffb28a2e33158f2d44422899467b67aa443aa34
Wilson Styres <wstyres@gmail.com>
2019-04-15 21:54:27 -0400

    [Database] Add DatabaseDelegate

---
+++ Zebra/Database/ZBDatabaseDelegate.h
@@ -0,0 +1,22 @@
+//
+//  ZBDatabaseDelegate.h
+//  Zebra
+//
+//  Created by Wilson Styres on 4/15/19.
+//  Copyright © 2019 Wilson Styres. All rights reserved.
+//
+
+#import <Console/ZBLogLevel.h>
+
+#ifndef ZBDatabaseDelegate_h
+#define ZBDatabaseDelegate_h
+
+@protocol ZBDatabaseDelegate
+- (void)databaseCompletedUpdate:(BOOL)success;
+@optional
+- (void)setRepo:(NSString *)bfn busy:(BOOL)busy;
+- (void)postStatusUpdate:(NSString *)status atLevel:(ZBLogLevel)level;
+@end
+
+
+#endif /* ZBDatabaseDelegate_h */
