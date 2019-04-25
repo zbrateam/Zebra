f9ef47aa63604e04e6bc8c8990eefb388a237926
Andrew Abosh <andrewaboshartworks@gmail.com>
2019-04-25 00:33:41 -0400

    [App] Add dummy quick actions

--- Zebra/ZBAppDelegate.m
+++ Zebra/ZBAppDelegate.m
@@ -206,6 +206,16 @@
     return true;
 }
 
+- (void)application:(UIApplication *)application performActionForShortcutItem:(UIApplicationShortcutItem *)shortcutItem completionHandler:(void (^)(BOOL))completionHandler {
+    // TODO: Make these actually work
+    if ([shortcutItem.type isEqualToString:@"Search"]) {
+        NSLog(@"Search Action Selected");
+    }
+    else if ([shortcutItem.type isEqualToString:@"Add"]) {
+        NSLog(@"Add Repo Action Selected");
+    }
+}
+
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
