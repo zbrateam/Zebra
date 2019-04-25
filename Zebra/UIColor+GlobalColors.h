ef9dd493d839a689981be98aa9e19c9ef192b54d
Andrew Abosh <andrewaboshartworks@gmail.com>
2019-04-24 23:29:00 -0400

    [UI] Add support for global color constants

---
+++ Zebra/UIColor+GlobalColors.h
@@ -0,0 +1,13 @@
+//
+//  UIColor+GlobalColors.h
+//  Zebra
+//
+//  Created by Andrew Abosh on 2019-04-24.
+//  Copyright © 2019 Wilson Styres. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIColor (GlobalColors)
++(UIColor*)tintColor;
+@end
---
+++ Zebra/UIColor+GlobalColors.m
@@ -0,0 +1,17 @@
+//
+//  UIColor+GlobalColors.m
+//  Zebra
+//
+//  Created by Andrew Abosh on 2019-04-24.
+//  Copyright © 2019 Wilson Styres. All rights reserved.
+//
+
+#import "UIColor+GlobalColors.h"
+
+@implementation UIColor (GlobalColors)
++(UIColor*)tintColor
+{
+    UIColor *color = [UIColor colorWithRed:0.51 green:0.13 blue:0.58 alpha:1.0];
+    return color;
+}
+@end
