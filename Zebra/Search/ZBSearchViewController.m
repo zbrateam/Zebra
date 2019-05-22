4975ce98d94ce316db098f161f4ae8c20f5b5e64
PoomSmart <thatchapon.unp@outlook.com>
2019-05-22 15:42:45 +0700

    [Search] Table view indexing is not needed for search.

--- Zebra/Search/ZBSearchViewController.m
+++ Zebra/Search/ZBSearchViewController.m
@@ -188,32 +188,6 @@
     return 5;
 }
 
-- (NSArray *)sectionIndexTitlesForTableView:(UITableView *)tableView {
-    if ((results.count == 0) || (results == nil))
-        return nil;
-    return [[UILocalizedIndexedCollation currentCollation] sectionIndexTitles];
-}
-
-- (NSInteger)tableView:(UITableView *)tableView sectionForSectionIndexTitle:(NSString *)title atIndex:(NSInteger)index {
-    if ((results.count == 0) || (results == nil))
-        return -1;
-    NSArray *titles = [self sectionIndexTitlesForTableView:tableView];
-    NSString *alphabet = titles[index];
-    dispatch_async(dispatch_get_main_queue(), ^{
-        int i = 0;
-        for (ZBPackage *package in self->results) {
-            if ([[package.name uppercaseString] hasPrefix:alphabet]) {
-                @try {
-                    [tableView scrollToRowAtIndexPath:[NSIndexPath indexPathForRow:i inSection:0] atScrollPosition:UITableViewScrollPositionTop animated:NO];
-                } @catch (NSException *e) {}
-                break;
-            }
-            ++i;
-        }
-    });
-    return -1;
-}
-
 #pragma mark - Swipe actions
 
 - (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
