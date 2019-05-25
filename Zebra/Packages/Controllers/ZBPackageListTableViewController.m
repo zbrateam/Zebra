6a8c29a2333bf36cb4043aaa9ca49632abe8a1ec
PoomSmart <thatchapon.unp@outlook.com>
2019-05-25 12:05:48 +0700

    [Packages] Show a number of upgrades.

--- Zebra/Packages/Controllers/ZBPackageListTableViewController.m
+++ Zebra/Packages/Controllers/ZBPackageListTableViewController.m
@@ -272,7 +272,7 @@
         UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(10, 0, tableView.frame.size.width - 10, 18)];
         [label setFont:[UIFont boldSystemFontOfSize:15]];
         if (isUpdateSection) {
-            [label setText:@"Available Upgrades"];
+            [label setText:[NSString stringWithFormat:@"Available Upgrades (%ld)", updates.count]];
         }
         else if (hasDataInSection) {
             [label setText:[self sectionIndexTitlesForTableView:tableView][[self trueSection:section]]];
