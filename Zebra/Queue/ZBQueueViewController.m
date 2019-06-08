ff9b42c43c4267a82e6b15813c2876bd2a5662e5
MidnightChip <midnightchips@gmail.com>
2019-06-08 17:03:27 -0600

    Yet another separator

--- Zebra/Queue/ZBQueueViewController.m
+++ Zebra/Queue/ZBQueueViewController.m
@@ -14,6 +14,7 @@
 
 @interface ZBQueueViewController () {
     ZBQueue *_queue;
+    NSUserDefaults *defaults;
 }
 
 @end
@@ -26,6 +27,12 @@
     _queue = [ZBQueue sharedInstance];
     
     self.navigationController.navigationBar.tintColor = [UIColor tintColor];
+    defaults = [NSUserDefaults standardUserDefaults];
+    if([defaults boolForKey:@"darkMode"]){
+        [self.tableView setSeparatorColor:[UIColor colorWithRed:0.2 green:0.2 blue:0.2 alpha:1.0]];
+    }else{
+        [self.tableView setSeparatorColor:[UIColor colorWithRed:0.784 green:0.784 blue:0.784 alpha:1.0]];
+    }
     
     [self refreshBarButtons];
     
