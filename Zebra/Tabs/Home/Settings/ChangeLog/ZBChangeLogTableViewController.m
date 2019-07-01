2e6e95c5761b1a3cc86351c1fe05ef46e79a2102
MidnightChip <midnightchips@gmail.com>
2019-06-30 21:54:39 -0600

    Fix incorrect enum

--- Zebra/Tabs/Home/Settings/ChangeLog/ZBChangeLogTableViewController.m
+++ Zebra/Tabs/Home/Settings/ChangeLog/ZBChangeLogTableViewController.m
@@ -18,7 +18,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     [self.navigationItem setTitle:@"Change Log"];
-    [self.tableView setSeparatorStyle:UITableViewCellSelectionStyleNone];
+    [self.tableView setSeparatorStyle:UITableViewCellSeparatorStyleNone];
 }
 
 - (void)viewWillAppear:(BOOL)animated {
