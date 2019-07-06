4705eb7b9265f92b9d1c815272ada17c9dc17c58
PoomSmart <thatchapon.unp@outlook.com>
2019-07-06 11:13:01 +0700

    [Theme] Fix alert and action sheet color in dark mode

--- Zebra/Extensions/UIAlertController+Theme.m
+++ Zebra/Extensions/UIAlertController+Theme.m
@@ -8,8 +8,32 @@
 
 #import "UIAlertController+Theme.h"
 #import <UIColor+GlobalColors.h>
+#import <objc/runtime.h>
 
 @implementation UIAlertController (Theme)
+
++ (void)load {
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        Class thisClass = self;
+        
+        SEL methodSEL = @selector(viewDidLayoutSubviews);
+        Method og_Method = class_getInstanceMethod(thisClass, methodSEL);
+        IMP methodIMP = method_getImplementation(og_Method);
+        
+        SEL mg_methodSEL = @selector(zb_viewDidLayoutSubviews);
+        Method mg_Method = class_getInstanceMethod(thisClass, mg_methodSEL);
+        IMP mg_methodIMP = method_getImplementation(mg_Method);
+        
+        BOOL wasMethodAdded = class_addMethod(thisClass, methodSEL, mg_methodIMP, method_getTypeEncoding(mg_Method));
+        
+        if (wasMethodAdded) {
+            class_replaceMethod(thisClass, mg_methodSEL, methodIMP, method_getTypeEncoding(og_Method));
+        } else {
+            method_exchangeImplementations(og_Method, mg_Method);
+        }
+    });
+}
 
 - (void)recursiveSetColor:(UIView *)view {
     NSArray *subviews = [view subviews];
@@ -22,25 +46,34 @@
         else if ([subview isKindOfClass:[UILabel class]]) {
             ((UILabel *)subview).textColor = [UIColor tintColor];
         }
-        else {
-            subview.backgroundColor = [UIColor cellBackgroundColor];
-        }
         [self recursiveSetColor:subview];
     }
 }
 
 - (void)setBackgroundColor {
-    for (UIView *groupView in self.view.subviews) {
+    UIView *view = self.view;
+    view.subviews[0].subviews[0].subviews[0].backgroundColor = [UIColor cellBackgroundColor];
+    for (UIView *groupView in view.subviews) {
         [self recursiveSetColor:groupView];
     }
+    do {
+        if ([view isKindOfClass:NSClassFromString(@"_UIPopoverView")]) {
+            UIView *backgroundView = [view valueForKey:@"_backgroundView"];
+            UIView *visualEffectView = [backgroundView valueForKey:@"_blurView"];
+            dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 0.1 * NSEC_PER_SEC), dispatch_get_main_queue(), ^(void) {
+                visualEffectView.subviews[1].backgroundColor = [UIColor cellBackgroundColor];
+            });
+            return;
+        }
+    } while ((view = view.superview));
 }
 
 - (void)setTextColor {
     self.view.tintColor = [UIColor tintColor];
 }
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
+- (void)zb_viewDidLayoutSubviews {
+    [self zb_viewDidLayoutSubviews];
     [self setBackgroundColor];
     [self setTextColor];
 }
