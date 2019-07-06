29526fe655503510aa3ab69af35125a547e5a551
MidnightChip <midnightchips@gmail.com>
2019-07-05 23:17:17 -0600

    [Meta] Zebra should never run as root

--- Zebra/main.m
+++ Zebra/main.m
@@ -7,28 +7,9 @@
 //
 
 #import <UIKit/UIKit.h>
-#import <dlfcn.h>
 #import "ZBAppDelegate.h"
 
-void patch_setuid() {
-    void* handle = dlopen("/usr/lib/libjailbreak.dylib", RTLD_LAZY);
-    if (!handle)
-        return;
-    
-    dlerror();
-    typedef void (*fix_setuid_prt_t)(pid_t pid);
-    fix_setuid_prt_t ptr = (fix_setuid_prt_t)dlsym(handle, "jb_oneshot_fix_setuid_now");
-    
-    const char *dlsym_error = dlerror();
-    if (dlsym_error)
-        return;
-    
-    ptr(getpid());
-}
-
 int main(int argc, char * argv[]) {
-    patch_setuid();
-    setuid(0);
     @autoreleasepool {
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([ZBAppDelegate class]));
     }
