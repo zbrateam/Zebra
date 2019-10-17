1213f532982a6a7c5d9140fa9f2de03d2f1350ca
Wilson Styres <wstyres@gmail.com>
2019-10-16 22:16:31 -0400

    [Repos] Add unc0ver's utility repo properly

--- Zebra/Tabs/Repos/Helpers/ZBRepoManager.m
+++ Zebra/Tabs/Repos/Helpers/ZBRepoManager.m
@@ -84,6 +84,7 @@
             @"apt.thebigboss.org/repofiles/cydia/",
             @"apt.modmyi.com/",
             @"apt.saurik.com/",
+            @"apt.bingner.com/",
             @"cydia.zodttd.com/",
             @"cydia.zodttd.com/repo/cydia/"];
     });
@@ -99,6 +100,7 @@
             @"deb http://apt.thebigboss.org/repofiles/cydia/ stable main\n",
             @"deb http://apt.modmyi.com/ stable main\n",
             [NSString stringWithFormat:@"deb http://apt.saurik.com/ ios/%.2f main\n", kCFCoreFoundationVersionNumber],
+            [NSString stringWithFormat:@"deb http://apt.bingner.com/ ios/%.2f main\n", kCFCoreFoundationVersionNumber],
             @"deb http://cydia.zodttd.com/repo/cydia/ stable main\n",
             @"deb http://cydia.zodttd.com/repo/cydia/ stable main\n"
         ];
