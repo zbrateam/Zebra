6734d198f556b4b71707673d02875488ab207123
Wilson Styres <wstyres@gmail.com>
2019-10-20 19:03:31 -0400

    [Packages] Re-enable queue and clear button

--- Zebra/Tabs/Packages/Controllers/ZBPackageListTableViewController.m
+++ Zebra/Tabs/Packages/Controllers/ZBPackageListTableViewController.m
@@ -198,18 +198,18 @@ typedef enum {
 }
 
 - (void)configureQueueOrShareButton {
-//    dispatch_async(dispatch_get_main_queue(), ^{
-//        if ([[ZBQueue sharedQueue] hasObjects]) {
-//            self->queueButton = [[UIBarButtonItem alloc] initWithTitle:@"Queue" style:UIBarButtonItemStylePlain target:self action:@selector(presentQueue)];
-//            self->clearButton = [[UIBarButtonItem alloc] initWithTitle:@"Clear" style:UIBarButtonItemStylePlain target:self action:@selector(askClearQueue)];
-//            self.navigationItem.leftBarButtonItems = @[ self->queueButton, self->clearButton ];
-//        } else {
-//            self->queueButton = self->clearButton = nil;
-//            self.navigationItem.leftBarButtonItems = nil;
-//            UIBarButtonItem *shareButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAction target:self action:@selector(sharePackages)];
-//            self.navigationItem.leftBarButtonItem = shareButton;
-//        }
-//    });
+    dispatch_async(dispatch_get_main_queue(), ^{
+        if ([ZBQueue count] > 0) {
+            self->queueButton = [[UIBarButtonItem alloc] initWithTitle:@"Queue" style:UIBarButtonItemStylePlain target:self action:@selector(presentQueue)];
+            self->clearButton = [[UIBarButtonItem alloc] initWithTitle:@"Clear" style:UIBarButtonItemStylePlain target:self action:@selector(askClearQueue)];
+            self.navigationItem.leftBarButtonItems = @[ self->queueButton, self->clearButton ];
+        } else {
+            self->queueButton = self->clearButton = nil;
+            self.navigationItem.leftBarButtonItems = nil;
+            UIBarButtonItem *shareButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAction target:self action:@selector(sharePackages)];
+            self.navigationItem.leftBarButtonItem = shareButton;
+        }
+    });
 }
 
 - (void)presentQueue {
