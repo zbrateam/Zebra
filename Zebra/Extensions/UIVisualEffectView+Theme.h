ccadae6bdf588ddfa08c33292b3689891b477263
Wilson Styres <wstyres@gmail.com>
2019-10-26 23:07:23 -0400

    [Theme] Swizzle UIVisualEffectView to set button colors

---
+++ Zebra/Extensions/UIVisualEffectView+Theme.h
@@ -0,0 +1,16 @@
+//
+//  UIVisualEffectView+Theme.h
+//  Zebra
+//
+//  Created by Wilson Styres on 10/26/19.
+//  Copyright © 2019 Wilson Styres. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UIVisualEffectView (Theme)
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Zebra/Extensions/UIVisualEffectView+Theme.m
@@ -0,0 +1,54 @@
+//
+//  UIVisualEffectView+Theme.m
+//  Zebra
+//
+//  Created by Wilson Styres on 10/26/19.
+//  Copyright © 2019 Wilson Styres. All rights reserved.
+//
+
+#import "UIVisualEffectView+Theme.h"
+#import "UIColor+GlobalColors.h"
+#import <ZBDevice.h>
+#import <objc/runtime.h>
+
+@implementation UIVisualEffectView (Theme)
+
++ (void)load {
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        Class thisClass = self;
+        
+        SEL methodSEL = @selector(layoutSubviews);
+        Method og_Method = class_getInstanceMethod(thisClass, methodSEL);
+        IMP methodIMP = method_getImplementation(og_Method);
+        
+        SEL mg_methodSEL = @selector(zb_layoutSubviews);
+        Method mg_Method = class_getInstanceMethod(thisClass, mg_methodSEL);
+        IMP mg_methodIMP = method_getImplementation(mg_Method);
+        
+        BOOL wasMethodAdded = class_addMethod(thisClass, methodSEL, mg_methodIMP, method_getTypeEncoding(mg_Method));
+        
+        if (wasMethodAdded) {
+            class_replaceMethod(thisClass, mg_methodSEL, methodIMP, method_getTypeEncoding(og_Method));
+        } else {
+            method_exchangeImplementations(og_Method, mg_Method);
+        }
+    });
+}
+
+- (void)setColor {
+    
+}
+
+- (void)zb_layoutSubviews {
+    NSLog(@"Swizzled!");
+//    [self layoutSubviews];
+    if ([ZBDevice darkModeEnabled] || [ZBDevice darkModeOledEnabled] || [ZBDevice darkModeThirteenEnabled]) {
+        self.effect = [UIBlurEffect effectWithStyle:UIBlurEffectStyleDark];
+    }
+    else {
+        self.effect = [UIBlurEffect effectWithStyle:UIBlurEffectStyleLight];
+    }
+}
+
+@end
