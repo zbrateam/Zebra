7dbc5248bb9cca330232e01ec4df0c5a248ac256
Wilson Styres <wstyres@gmail.com>
2019-11-08 15:21:52 -0500

    [Delegate] Main queue only please

--- Zebra/ZBAppDelegate.m
+++ Zebra/ZBAppDelegate.m
@@ -119,7 +119,9 @@ static const NSInteger kZebraMaxTime = 60 * 60 * 24; // 1 day
 }
 
 + (ZBTabBarController *)tabBarController {
-    return (ZBTabBarController *)((ZBAppDelegate *)[[UIApplication sharedApplication] delegate]).window.rootViewController;
+    dispatch_async(dispatch_get_main_queue(), ^{
+        return (ZBTabBarController *)((ZBAppDelegate *)[[UIApplication sharedApplication] delegate]).window.rootViewController;
+    });
 }
 
 + (void)sendAlertFrom:(UIViewController *)vc title:(NSString *)title message:(NSString *)message actionLabel:(NSString *)actionLabel okLabel:(NSString *)okLabel block:(void (^)(void))block {
