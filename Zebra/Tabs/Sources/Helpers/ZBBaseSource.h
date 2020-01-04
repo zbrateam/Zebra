317666f943cb772d120e0c2f1344b9e806789b2b
Wilson Styres <wstyres@gmail.com>
2020-01-04 12:24:53 -0500

    [Sources] Document ZBBaseSource a bit

--- Zebra/Tabs/Sources/Helpers/ZBBaseSource.h
+++ Zebra/Tabs/Sources/Helpers/ZBBaseSource.h
@@ -11,13 +11,36 @@
 NS_ASSUME_NONNULL_BEGIN
 
 @interface ZBBaseSource : NSObject
+
+/*!
+ @brief The archive type
+ @discussion Indicates the type of archive. Deb indicates that the archive contains binary packages (deb), the pre-compiled packages that we normally use. Deb-src indicates source packages, which are the original program sources plus the Debian control file (.dsc) and the diff.gz containing the changes needed for packaging the program.
+ @remark Though deb-src is an available option, Zebra does not currently support it.
+ */
 @property (nonatomic) NSString *archiveType;
+
+/*! @brief URL to the repository that you want to download the packages from. */
 @property (nonatomic) NSString *repositoryURI;
+
+/*!
+    @brief Repository distribution
+    @discussion The 'distribution' can be either the release code name / alias or the release class respectively. Most iOS APT repositories opt to use the "Flat Repository Format" with a distribution of ./
+*/
 @property (nonatomic) NSString *distribution;
+
+/*!
+    @brief Components of a repository
+    @remark Most iOS APT repositories opt to not have any components
+ */
 @property (nonatomic) NSArray <NSString *> *_Nullable components;
 
+/*! @brief A URL generated that points to the directory where all the components for the repository should be located */
 @property (nonatomic) NSURL *mainDirectoryURL;
+
+/*! @brief A URL generated that points to the directory where all the Packages files for the repository should be located */
 @property (nonatomic) NSURL *packagesDirectoryURL;
+
+/*! @brief A URL generated that points to the directory where the Release file for the repository should be located */
 @property (nonatomic) NSURL *releaseURL;
 
 /*! @brief An identifier of a NSURLSessionDownloadTask to retrieve information about the task downloading the repository's release file */
@@ -44,6 +67,7 @@ NS_ASSUME_NONNULL_BEGIN
 + (NSSet <ZBBaseSource *> *)baseSourcesFromList:(NSString *)listPath error:(NSError **)error;
 - (id)initWithArchiveType:(NSString *)archiveType repositoryURI:(NSString *)repositoryURI distribution:(NSString *)distribution components:(NSArray <NSString *> *_Nullable)components;
 - (id)initFromSourceLine:(NSString *)debLine;
+- (BOOL)verify;
 - (NSString *)debLine;
 - (BOOL)canDelete;
 - (BOOL)isEqual:(ZBBaseSource *)object;
