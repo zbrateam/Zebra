ea42d740396847ecd9cba27871cd0d24ee431d8e
Wilson Styres <wstyres@gmail.com>
2020-01-04 21:06:40 -0500

    [Packages] Fix crash when not removing scrollView observer

--- Zebra/Tabs/Packages/Controllers/ZBPackageDepictionViewController.m
+++ Zebra/Tabs/Packages/Controllers/ZBPackageDepictionViewController.m
@@ -136,8 +136,6 @@ static const NSUInteger ZBPackageInfoOrderCount = 8;
     } else {
         [self prepDepictionLoading:[[NSBundle mainBundle] URLForResource:@"package_depiction" withExtension:@"html"]];
     }
-    [webView addObserver:self forKeyPath:NSStringFromSelector(@selector(estimatedProgress)) options:NSKeyValueObservingOptionNew context:NULL];
-    [webView.scrollView addObserver:self forKeyPath:NSStringFromSelector(@selector(contentSize)) options:NSKeyValueObservingOptionNew context:NULL];
     
     CLS_LOG(@"%@ (%@) from %@", [package name], [package identifier], [[package repo] baseURL]);
 }
@@ -147,9 +145,17 @@ static const NSUInteger ZBPackageInfoOrderCount = 8;
     self.tableView.separatorColor = [UIColor cellSeparatorColor];
     [self configureNavButton];
     
+    [webView addObserver:self forKeyPath:NSStringFromSelector(@selector(estimatedProgress)) options:NSKeyValueObservingOptionNew context:NULL];
+    [webView.scrollView addObserver:self forKeyPath:NSStringFromSelector(@selector(contentSize)) options:NSKeyValueObservingOptionNew context:NULL];
+    
     if (@available(iOS 11.0, *)) {
         self.navigationItem.largeTitleDisplayMode = UINavigationItemLargeTitleDisplayModeNever;
     }
+}
+
+- (void)viewWillDisappear:(BOOL)animated {
+    [webView removeObserver:self forKeyPath:NSStringFromSelector(@selector(estimatedProgress)) context:nil];
+    [webView.scrollView removeObserver:self forKeyPath:NSStringFromSelector(@selector(contentSize)) context:nil];
 }
 
 - (void)prepDepictionLoading:(NSURL *)url {
@@ -623,11 +629,6 @@ static const NSUInteger ZBPackageInfoOrderCount = 8;
     
     alert.popoverPresentationController.barButtonItem = self.navigationItem.rightBarButtonItem;
     [self presentViewController:alert animated:YES completion:nil];
-}
-
-- (void)dealloc {
-    [webView removeObserver:self forKeyPath:NSStringFromSelector(@selector(estimatedProgress)) context:nil];
-    [webView.scrollView removeObserver:self forKeyPath:NSStringFromSelector(@selector(contentSize)) context:nil];
 }
 
 - (void)presentQueue {
