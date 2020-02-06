4937a4a24698d105a8db1b0045ee66df29c21252
Wilson Styres <wstyres@gmail.com>
2020-02-05 21:51:03 -0500

    [Settings] Implement picking of accent color

--- Zebra/Tabs/Home/Settings/Display/Accent Color/ZBAccentColorTableViewController.m
+++ Zebra/Tabs/Home/Settings/Display/Accent Color/ZBAccentColorTableViewController.m
@@ -75,48 +75,20 @@
     return cell;
 }
 
-/*
-// Override to support conditional editing of the table view.
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    [tableView deselectRowAtIndexPath:indexPath animated:true];
+    
+    UITableViewCell *oldCell = [tableView cellForRowAtIndexPath:[NSIndexPath indexPathForRow:[colors indexOfObject:@(selectedColor)] inSection:0]];
+    [oldCell setAccessoryType:UITableViewCellAccessoryNone];
+    
+    ZBAccentColor newColor = (ZBAccentColor)[colors[indexPath.row] integerValue];
+    selectedColor = newColor;
+    
+    [ZBSettings setAccentColor:newColor];
+    
+    UITableViewCell *newCell = [tableView cellForRowAtIndexPath:indexPath];
+    [newCell setTintColor:[UIColor accentColor]];
+    [newCell setAccessoryType:UITableViewCellAccessoryCheckmark];
 }
-*/
-
-/*
-// Override to support editing the table view.
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
-}
-*/
-
-/*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
-}
-*/
-
-/*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
