0a0f79602db4efc0c2e8f76547ec760b14656dc8
Wilson Styres <wstyres@gmail.com>
2020-02-23 19:29:08 -0500

    [Packages] Add ZBProxyPackage

---
+++ Zebra/Tabs/Packages/Helpers/ZBProxyPackage.h
@@ -0,0 +1,17 @@
+//
+//  ZBProxyPackage.h
+//  Zebra
+//
+//  Created by Wilson Styres on 2/23/20.
+//  Copyright © 2020 Wilson Styres. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ZBProxyPackage : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Zebra/Tabs/Packages/Helpers/ZBProxyPackage.m
@@ -0,0 +1,13 @@
+//
+//  ZBProxyPackage.m
+//  Zebra
+//
+//  Created by Wilson Styres on 2/23/20.
+//  Copyright © 2020 Wilson Styres. All rights reserved.
+//
+
+#import "ZBProxyPackage.h"
+
+@implementation ZBProxyPackage
+
+@end
