46619c16853394f33030f9c8ebcd3876ddf368bc
Wilson Styres <wstyres@gmail.com>
2020-02-23 20:17:59 -0500

    [Packages] Add methods for proxy package

--- Zebra/Tabs/Packages/Helpers/ZBProxyPackage.h
+++ Zebra/Tabs/Packages/Helpers/ZBProxyPackage.h
@@ -6,12 +6,29 @@
 //  Copyright © 2020 Wilson Styres. All rights reserved.
 //
 
+@class ZBPackage;
+
 #import <Foundation/Foundation.h>
+#import <sqlite3.h>
 
 NS_ASSUME_NONNULL_BEGIN
 
 @interface ZBProxyPackage : NSObject
 
+//Identifying properties
+@property (nonatomic) NSString *name;
+@property (nonatomic) NSString *identifier;
+@property (nonatomic) NSString *version;
+@property (nonatomic) int repoID;
+
+//Extra properties for display
+@property (nonatomic) NSString *author;
+@property (nonatomic) NSURL *iconURL;
+@property (nonatomic) NSString *section;
+
+@property (nonatomic) ZBPackage *package;
+- (id)initWithSQLiteStatement:(sqlite3_stmt *)statement;
+- (id)forwardingTargetForSelector:(SEL)selector;
 @end
 
 NS_ASSUME_NONNULL_END
--- Zebra/Tabs/Packages/Helpers/ZBProxyPackage.m
+++ Zebra/Tabs/Packages/Helpers/ZBProxyPackage.m
@@ -8,6 +8,38 @@
 
 #import "ZBProxyPackage.h"
 
+#import <Database/ZBDatabaseManager.h>
+
 @implementation ZBProxyPackage
+
+@synthesize name;
+@synthesize identifier;
+@synthesize version;
+@synthesize repoID;
+
+@synthesize author;
+@synthesize iconURL;
+@synthesize section;
+
+@synthesize package;
+
+- (id)initWithSQLiteStatement:(sqlite3_stmt *)statement {
+    self = [super init];
+    
+    if (self) {
+        
+    }
+    
+    return self;
+}
+
+- (id)forwardingTargetForSelector:(SEL)selector {
+    if (package) return package;
+    
+    ZBDatabaseManager *databaseManager = [ZBDatabaseManager sharedInstance];
+    package = [databaseManager packageFromProxy:self];
+    
+    return package;
+}
 
 @end
