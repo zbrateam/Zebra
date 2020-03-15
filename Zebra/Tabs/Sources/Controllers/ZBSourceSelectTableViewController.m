420f2ef863cec6e1b84f97c1b2dc5f182c09398f
Wilson Styres <wstyres@gmail.com>
2020-03-15 13:50:52 -0400

    [Sources] Make source selection actually work

--- Zebra/Tabs/Sources/Controllers/ZBSourceSelectTableViewController.m
+++ Zebra/Tabs/Sources/Controllers/ZBSourceSelectTableViewController.m
@@ -13,7 +13,10 @@
 #import "ZBSource.h"
 #import "UIColor+GlobalColors.h"
 
-@interface ZBSourceSelectTableViewController ()
+@interface ZBSourceSelectTableViewController () {
+    ZBSource *selectedSource;
+    NSIndexPath *selectedIndex;
+}
 
 @end
 
@@ -38,7 +41,8 @@
 - (void)layoutNavigationButtonsNormal {
     self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"Cancel" style:UIBarButtonItemStylePlain target:self action:@selector(goodbye)];
     
-    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"Done" style:UIBarButtonItemStyleDone target:self action:@selector(layoutNavigationButtonsNormal)];
+    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"Add" style:UIBarButtonItemStyleDone target:self action:@selector(layoutNavigationButtonsNormal)];
+    self.navigationItem.rightBarButtonItem.enabled = selectedSource;
 }
 
 - (void)checkClipboard {}
@@ -71,48 +75,24 @@
     return cell;
 }
 
-/*
-// Override to support conditional editing of the table view.
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    [tableView deselectRowAtIndexPath:indexPath animated:true];
+    
+    dispatch_async(dispatch_get_main_queue(), ^{
+        if (self->selectedIndex && ![self->selectedIndex isEqual:indexPath]) {
+            UITableViewCell *oldCell = [tableView cellForRowAtIndexPath:self->selectedIndex];
+            oldCell.accessoryType = UITableViewCellAccessoryNone;
+        }
+        
+        UITableViewCell *newCell = [tableView cellForRowAtIndexPath:indexPath];
+        newCell.accessoryType = UITableViewCellAccessoryCheckmark;
+        
+        self->selectedIndex = indexPath;
+        self->selectedSource = [self sourceAtIndexPath:self->selectedIndex];
+        
+        [self layoutNavigationButtonsNormal];
+    });
 }
-*/
 
-/*
-// Override to support editing the table view.
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
-}
-*/
-
-/*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
-}
-*/
-
-/*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
