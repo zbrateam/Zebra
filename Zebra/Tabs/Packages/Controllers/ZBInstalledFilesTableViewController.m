00c2af3728853141353f5a5000f53cb94a977bcb
Andrew Abosh <andrewaboshartworks@gmail.com>
2020-04-27 20:38:53 -0400

    [Packages] Correctly display Installed Files for RTL languages

--- Zebra/Tabs/Packages/Controllers/ZBInstalledFilesTableViewController.m
+++ Zebra/Tabs/Packages/Controllers/ZBInstalledFilesTableViewController.m
@@ -23,6 +23,7 @@
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(reloadTableView) name:@"darkMode" object:nil];
     files = [NSMutableArray new];
     [self getInstalledFiles];
+    self.tableView.tableFooterView = [[UIView alloc] initWithFrame:CGRectZero];
 }
 
 - (void)viewWillAppear:(BOOL)animated {
@@ -45,7 +46,12 @@
         for (int b = 0; b < components.count - 2; ++b) {
             [displayStr appendString:@"\t"]; // add tab character
         }
-        [displayStr appendString:components[components.count - 1]];
+        if ([UIApplication sharedApplication].userInterfaceLayoutDirection == UIUserInterfaceLayoutDirectionLeftToRight) {
+            [displayStr appendString:components[components.count - 1]];
+        } else {
+            [displayStr insertString:components[components.count - 1] atIndex:0];
+        }
+        
         [files addObject:displayStr];
     }
 }
