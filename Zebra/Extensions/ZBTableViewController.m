a52ad7704bc0124e04578ccc701d76bce7a76415
PoomSmart <thatchapon.unp@outlook.com>
2020-05-09 14:52:58 +0700

    [Theme] Fix up table header footer view color

--- Zebra/Extensions/ZBTableViewController.m
+++ Zebra/Extensions/ZBTableViewController.m
@@ -66,6 +66,10 @@
     }
 }
 
+- (void)tableView:(UITableView *)tableView willDisplayHeaderView:(UIView *)view forSection:(NSInteger)section {
+    view.backgroundColor = [UIColor groupedTableViewBackgroundColor];
+}
+
 - (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
     return UITableViewAutomaticDimension;
 }
