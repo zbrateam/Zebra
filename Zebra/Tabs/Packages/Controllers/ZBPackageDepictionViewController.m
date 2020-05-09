5f4219773c3e410bebf14814dfbc0dc7bacdad5d
Andrew Abosh <andrewaboshartworks@gmail.com>
2020-05-08 22:14:15 -0400

    [Packages] Set data of the depiction

--- Zebra/Tabs/Packages/Controllers/ZBPackageDepictionViewController.m
+++ Zebra/Tabs/Packages/Controllers/ZBPackageDepictionViewController.m
@@ -7,9 +7,12 @@
 //
 
 #import "ZBPackageDepictionViewController.h"
+#import <Packages/Helpers/ZBPackage.h>
 
 @interface ZBPackageDepictionViewController ()
 @property (strong, nonatomic) IBOutlet UIImageView *iconImageView;
+@property (weak, nonatomic) IBOutlet UILabel *nameLabel;
+@property (weak, nonatomic) IBOutlet UILabel *shortDescriptionLabel;
 @property (strong, nonatomic) IBOutlet UIButton *getButton;
 @property (strong, nonatomic) IBOutlet UIButton *moreButton;
 @property (strong, nonatomic) IBOutlet WKWebView *webView;
@@ -32,20 +35,34 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    [self setData];
+    [self applyCustomizations];
+    [self setDelegates];
+}
+
+- (void)setData {
+    self.nameLabel.text = self.package.name;
+    self.shortDescriptionLabel.text = self.package.shortDescription;
+    [self.package setIconImageForImageView:self.iconImageView];
+        
+    [self.webView loadRequest:[NSURLRequest requestWithURL:self.package.depictionURL]];
+}
+
+- (void)applyCustomizations {
+    // Package Icon
     self.iconImageView.layer.cornerRadius = 20;
     self.iconImageView.layer.borderWidth = 1;
     self.iconImageView.layer.borderColor = [[UIColor colorWithRed: 0.90 green: 0.90 blue: 0.92 alpha: 1.00] CGColor]; // TODO: Don't hardcode
-    self.iconImageView.image = [UIImage imageNamed:@"icon"];
     
+    // Buttons
     self.getButton.layer.cornerRadius = self.getButton.frame.size.height / 2;
     self.moreButton.layer.cornerRadius = self.moreButton.frame.size.height / 2;
     
     self.webView.hidden = YES;
+}
+
+- (void)setDelegates {
     self.webView.navigationDelegate = self;
-    
-    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0), ^{
-        [self.webView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"https://getzbra.com/repo/depictions/xyz.willy.Zebra/"]]];
-    });
 }
 
 - (void)webView:(WKWebView *)webView didFinishNavigation:(WKNavigation *)navigation {
