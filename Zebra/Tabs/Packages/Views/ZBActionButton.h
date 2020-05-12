59eab7a096b09522a9c940c5a3a3fdb0d2e288fd
Andrew Abosh <andrewaboshartworks@gmail.com>
2020-05-11 23:11:29 -0400

    [Packages] Create ZBActionButton

---
+++ Zebra/Tabs/Packages/Views/ZBActionButton.h
@@ -0,0 +1,16 @@
+//
+//  ZBActionButton.h
+//  Zebra
+//
+//  Created by Andrew Abosh on 2020-05-11.
+//  Copyright © 2020 Wilson Styres. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ZBActionButton : UIButton
+@end
+
+NS_ASSUME_NONNULL_END
