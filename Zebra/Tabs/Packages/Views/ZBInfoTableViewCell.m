526936627dbb3887a56cb64c7bbab758ae184c2c
Andrew Abosh <andrewaboshartworks@gmail.com>
2020-05-12 22:52:40 -0400

    [Packages] Create and use ZBInfoTableViewCell

---
+++ Zebra/Tabs/Packages/Views/ZBInfoTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  ZBInfoTableViewCell.m
+//  Zebra
+//
+//  Created by Andrew Abosh on 2020-05-12.
+//  Copyright © 2020 Wilson Styres. All rights reserved.
+//
+
+#import "ZBInfoTableViewCell.h"
+
+@implementation ZBInfoTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
