39fa374ea8e60dc0a02d208f678052b73d64996b
Wilson Styres <wstyres@gmail.com>
2020-05-17 21:23:37 -0400

    [Extensions] Actually use the custom attributes for markdown

--- Zebra/Extensions/NSAttributedString+Markdown.h
+++ Zebra/Extensions/NSAttributedString+Markdown.h
@@ -7,11 +7,13 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
 NS_ASSUME_NONNULL_BEGIN
 
 @interface NSAttributedString (Markdown)
 - (id)initWithMarkdownString:(NSString *)markdownString;
+- (id)initWithMarkdownString:(NSString *)markdownString fontSize:(CGFloat)fontSize;
 @end
 
 NS_ASSUME_NONNULL_END
--- Zebra/Extensions/NSAttributedString+Markdown.m
+++ Zebra/Extensions/NSAttributedString+Markdown.m
@@ -15,13 +15,36 @@
 - (id)initWithMarkdownString:(NSString *)markdownString {
     if (markdownString == nil) return nil;
     CMDocument *document = [[CMDocument alloc] initWithString:markdownString options:0];
+
+    CMAttributedStringRenderer *renderer = [[CMAttributedStringRenderer alloc] initWithDocument:document attributes:[self baseAttributes:0]];
+    
+    return [renderer render];
+}
+
+- (id)initWithMarkdownString:(NSString *)markdownString fontSize:(CGFloat)fontSize {
+    if (markdownString == nil) return nil;
+    CMDocument *document = [[CMDocument alloc] initWithString:markdownString options:0];
+    
+    CMAttributedStringRenderer *renderer = [[CMAttributedStringRenderer alloc] initWithDocument:document attributes:[self baseAttributes:fontSize]];
+    
+    return [renderer render];
+}
+
+- (CMTextAttributes *)baseAttributes:(CGFloat)fontSize {
     CMTextAttributes *attributes = [[CMTextAttributes alloc] init];
+    
+    // Set spacing on ordered lists
+    [attributes addParagraphStyleAttributes:@{CMParagraphStyleAttributeFirstLineHeadExtraIndent: @(0), CMParagraphStyleAttributeHeadExtraIndent: @(0)} forElementWithKinds:CMElementKindOrderedList];
+    [attributes addParagraphStyleAttributes:@{CMParagraphStyleAttributeFirstLineHeadExtraIndent: @(0), CMParagraphStyleAttributeHeadExtraIndent: @(0)} forElementWithKinds:CMElementKindOrderedSublist];
+    
+    // Set spacing on unordered list (and set bullet character)
     [attributes addParagraphStyleAttributes:@{CMParagraphStyleAttributeListItemBulletString: @"\u2022", CMParagraphStyleAttributeFirstLineHeadExtraIndent: @(0), CMParagraphStyleAttributeHeadExtraIndent: @(0)} forElementWithKinds:CMElementKindUnorderedList];
     [attributes addParagraphStyleAttributes:@{CMParagraphStyleAttributeListItemBulletString: @"\u25E6", CMParagraphStyleAttributeFirstLineHeadExtraIndent: @(0), CMParagraphStyleAttributeHeadExtraIndent: @(0)} forElementWithKinds:CMElementKindUnorderedSublist];
     
-    CMAttributedStringRenderer *renderer = [[CMAttributedStringRenderer alloc] initWithDocument:document attributes:[[CMTextAttributes alloc] init]];
+    // Set font size
+    if (fontSize > 0) [attributes addFontAttributes:@{UIFontDescriptorSizeAttribute: @(fontSize)} forElementWithKinds:CMElementKindText];
     
-    return [renderer render];
+    return attributes;
 }
 
 @end
