91d8a4ac3c3d85358a22ba81eb4673f90803c91a
eve0415 <40632436+eve0415@users.noreply.github.com>
2020-06-12 20:51:16 +0900

    [Filter] Improve filter setting - author (#1415)
    * [Filter] Disable add button when new author isn't selected
    
    * Hide infomation button when there are no aliases for the author
    
    * Don't remove text when pressing seach button
    
    * Dismiss View Controller properly when pressed cancel

--- Zebra/Tabs/Home/Settings/Filters/ZBAuthorSelectorTableViewController.m
+++ Zebra/Tabs/Home/Settings/Filters/ZBAuthorSelectorTableViewController.m
@@ -18,6 +18,7 @@
     ZBDatabaseManager *databaseManager;
     NSArray <NSArray <NSString *> *> *authors;
     NSMutableDictionary <NSString *, NSString *> *selectedAuthors;
+    NSMutableArray *newSelectedAuthors;
     BOOL shouldPerformSearching;
 }
 @end
@@ -33,6 +34,7 @@
     
     if (self) {
         authors = @[];
+        newSelectedAuthors = [NSMutableArray new];
         selectedAuthors = [[ZBSettings blockedAuthors] mutableCopy];
     }
     
@@ -109,7 +111,7 @@
     self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:NSLocalizedString(@"Cancel", @"") style:UIBarButtonItemStylePlain target:self action:@selector(goodbye)];
     
     self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:NSLocalizedString(@"Add", @"") style:UIBarButtonItemStyleDone target:self action:@selector(addAuthors)];
-    self.navigationItem.rightBarButtonItem.enabled = [[selectedAuthors allKeys] count];
+    self.navigationItem.rightBarButtonItem.enabled = [newSelectedAuthors count];
 }
 
 - (void)addAuthors {
@@ -125,6 +127,9 @@
 }
 
 - (void)goodbye {
+    if (searchController.active) {
+        [searchController setActive:NO];
+    }
     [self dismissViewControllerAnimated:YES completion:nil];
 }
 
@@ -176,7 +181,6 @@
     self->shouldPerformSearching = YES;
     
     [self updateSearchResultsForSearchController:searchController];
-    [self.searchController setActive:NO];
 }
 
 #pragma mark - Table View Data Source
@@ -220,9 +224,11 @@
     NSArray <NSString *> *authorDetail = authors[indexPath.row];
     if (selectedAuthors[authorDetail[1]]) {
         [selectedAuthors removeObjectForKey:authorDetail[1]];
+        [newSelectedAuthors removeObject:authorDetail[1]];
     }
     else if (authorDetail[1].length) {
         [selectedAuthors setObject:authorDetail[0] forKey:authorDetail[1]];
+        [newSelectedAuthors addObject:authorDetail[1]];
     }
     
     [self refreshTable];
