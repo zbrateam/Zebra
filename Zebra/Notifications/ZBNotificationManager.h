0fbdd3ed3d76b2cd4d98a248cad28f180cd839aa
Arthur Chaloin <arthur@achaloin.com>
2020-08-16 20:08:55 +0200

    [Notifications] Preliminary support for push notifications on package updates (#1399)
    * [Notifications] Cleaner implementation of package updates notifications
    
    * [Notifications] Style improvement
    
    * [Notifications] Use caching
    
    * [Notifications] Localization
    
    * [Notifications] Update bulletin identifier
    
    * [Notifications] Send a single notification for multiple updates

---
+++ Zebra/Notifications/ZBNotificationManager.h
@@ -0,0 +1,33 @@
+//
+//  ZBNotificationManager.h
+//  Zebra
+//
+//  Created by Arthur Chaloin on 06/06/2020.
+//  Copyright © 2020 Wilson Styres. All rights reserved.
+//
+
+#ifndef ZBNotificationManager_h
+#define ZBNotificationManager_h
+
+#import <UIKit/UIKit.h>
+#import <UserNotifications/UserNotifications.h>
+#import <ZBDatabaseDelegate.h>
+#import <ZBPackage.h>
+
+typedef void (^BackgroundCompletionHandler)(UIBackgroundFetchResult);
+
+typedef NSMutableArray<ZBPackage *> ZBPackageList;
+
+@interface ZBNotificationManager : NSObject <ZBDatabaseDelegate, UNUserNotificationCenterDelegate>
+
+- (void)ensureNotificationAccess;
+- (void)performBackgroundFetch:(nonnull BackgroundCompletionHandler)completionHandler;
+- (UIBackgroundFetchResult)notifyNewUpdatesBetween:(nonnull ZBPackageList *)oldUpdates newUpdates:(nonnull ZBPackageList *)newUpdates;
+- (void)notifyUpdateForPackages:(nonnull ZBPackageList *)packages;
+- (void)notify:(nonnull NSString *)body withTitle:(nonnull NSString *)title withUserInfo:(nonnull NSDictionary *)userInfo;
+
++ (nonnull instancetype)sharedInstance;
+
+@end
+
+#endif /* ZBNotificationManager_h */
---
+++ Zebra/Notifications/ZBNotificationManager.m
@@ -0,0 +1,185 @@
+//
+//  ZBNotificationManager.m
+//  Zebra
+//
+//  Created by Arthur Chaloin on 06/06/2020.
+//  Copyright © 2020 Wilson Styres. All rights reserved.
+//
+
+#import "ZBNotificationManager.h"
+#import <ZBDatabaseManager.h>
+#import <ZBSource.h>
+
+@interface ZBNotificationManager ()
+
+@property () BackgroundCompletionHandler completionHandler;
+@property () ZBPackageList *oldUpdates;
+
+- (void)fetchCompleted:(UIBackgroundFetchResult)result;
+
+@end
+
+@implementation ZBNotificationManager
+
+- (void)ensureNotificationAccess {
+    UNUserNotificationCenter *center = [UNUserNotificationCenter currentNotificationCenter];
+    
+    [center requestAuthorizationWithOptions:(UNAuthorizationOptionAlert | UNAuthorizationOptionBadge) completionHandler:^(BOOL granted, NSError * _Nullable error) {
+        if (error) {
+            NSLog(@"[Zebra] Error: %@", error.localizedDescription);
+        } else if (!granted) {
+            NSLog(@"[Zebra] Authorization was not granted.");
+        } else {
+            NSLog(@"[Zebra] Notification access granted.");
+        }
+    }];
+    
+    center.delegate = self;
+}
+
+- (void)performBackgroundFetch:(BackgroundCompletionHandler)completionHandler {
+    ZBDatabaseManager *databaseManager = [ZBDatabaseManager sharedInstance];
+    
+    self.completionHandler = completionHandler;
+    self.oldUpdates = [databaseManager packagesWithUpdates];
+
+    [databaseManager updateDatabaseUsingCaching:YES userRequested:YES];
+}
+
+- (UIBackgroundFetchResult)notifyNewUpdatesBetween:(ZBPackageList *)oldUpdates
+                                        newUpdates:(ZBPackageList *)newUpdates {
+    UIBackgroundFetchResult result = UIBackgroundFetchResultNoData;
+    ZBPackageList *packagesToNotify = [ZBPackageList new];
+    
+    for (ZBPackage *package in newUpdates) {
+        NSPredicate *predicate = [NSPredicate predicateWithFormat:@"identifier == %@", package.identifier];
+        NSArray<ZBPackage *> *filteredPackages = [oldUpdates filteredArrayUsingPredicate:predicate];
+        
+        if (filteredPackages.count > 1) {
+            NSLog(@"WARNING: Received multiple updates for the same package. This is most probably a developer error.");
+            continue;
+        }
+        else if (filteredPackages.count <= 0) {
+            [packagesToNotify addObject:package];
+        }
+        else {
+            ZBPackage *oldPackage = filteredPackages[0];
+            
+            if (![package.version isEqualToString:oldPackage.version]) {
+                [packagesToNotify addObject:package];
+            }
+        }
+    }
+
+    if (packagesToNotify.count > 0) {
+        [self notifyUpdateForPackages:packagesToNotify];
+        result = UIBackgroundFetchResultNewData;
+    }
+
+    return result;
+}
+
+- (void)notifyUpdateForPackages:(ZBPackageList *)packages {
+    if (packages.count == 1) {
+        ZBPackage *package = packages[0];
+
+        NSString *title = [NSString stringWithFormat:NSLocalizedString(@"Update available for %@", @""), package.name];
+        NSString *text = [NSString stringWithFormat:NSLocalizedString(@"Version %@ is available on %@.", @""), package.version, [package.source label]];
+
+        [self notify:text withTitle:title withUserInfo:@{
+            @"openURL": [NSString stringWithFormat:@"zbra://packages/%@", package.identifier],
+        }];
+    }
+    else if (packages.count > 1) {
+        NSString *title = [NSString stringWithFormat:NSLocalizedString(@"%d updates available", @""), packages.count];
+        NSString *text = [NSString stringWithFormat:NSLocalizedString(@"%@ and %d more can be updated.", @""), packages[0].name, packages.count - 1];
+
+        [self notify:text withTitle:title withUserInfo:@{
+            @"openURL": @"zbra://changes",
+        }];
+    }
+}
+
+- (void)notify:(NSString *)body withTitle:(NSString *)title withUserInfo:(NSDictionary *)userInfo {
+    NSDate* now = [NSDate date];
+    NSCalendar *calendar = [[NSCalendar alloc] initWithCalendarIdentifier:NSCalendarIdentifierGregorian];
+    NSDateComponents *date = [calendar components:NSCalendarUnitYear|NSCalendarUnitMonth|NSCalendarUnitDay|NSCalendarUnitHour|NSCalendarUnitMinute|NSCalendarUnitSecond|NSCalendarUnitTimeZone fromDate:[now dateByAddingTimeInterval:3]];
+
+    UNCalendarNotificationTrigger* trigger = [UNCalendarNotificationTrigger
+           triggerWithDateMatchingComponents:date repeats:NO];
+
+    UNMutableNotificationContent* content = [[UNMutableNotificationContent alloc] init];
+    content.title = title;
+    content.body = body;
+    content.userInfo = userInfo;
+
+    UNNotificationRequest* request = [UNNotificationRequest
+           requestWithIdentifier:@"xyz.willy.Zebra.updates" content:content trigger:trigger];
+
+    UNUserNotificationCenter* center = [UNUserNotificationCenter currentNotificationCenter];
+    [center addNotificationRequest:request withCompletionHandler:^(NSError * _Nullable error) {
+       if (error != nil) {
+           NSLog(@"%@", error.localizedDescription);
+       }
+    }];
+}
+
+- (void)fetchCompleted:(UIBackgroundFetchResult)result {
+    self.oldUpdates = nil;
+
+    if (self.completionHandler != nil) {
+        BackgroundCompletionHandler completionHandler = self.completionHandler;
+        self.completionHandler = nil;
+        completionHandler(result);
+    }
+}
+
+#pragma mark ZBDatabaseDelegate
+
+- (void)databaseCompletedUpdate:(int)numberOfUpdates {
+    if (numberOfUpdates <= 0) {
+        [self fetchCompleted:UIBackgroundFetchResultNoData];
+        return;
+    }
+
+    ZBDatabaseManager *databaseManager = [ZBDatabaseManager sharedInstance];
+    ZBPackageList *newUpdates = [databaseManager packagesWithUpdates];
+
+    UIBackgroundFetchResult result = [self notifyNewUpdatesBetween:self.oldUpdates newUpdates:newUpdates];
+    [self fetchCompleted:result];
+}
+
+- (void)databaseStartedUpdate {}
+
+#pragma mark UNUserNotificationCenterDelegate
+
+- (void)userNotificationCenter:(UNUserNotificationCenter *)center didReceiveNotificationResponse:(nonnull UNNotificationResponse *)response withCompletionHandler:(nonnull void (^)(void))completionHandler {
+    
+    NSDictionary *userInfo = response.notification.request.content.userInfo;
+    NSURL *openURL = [NSURL URLWithString:[userInfo objectForKey:@"openURL"]];
+    
+    if (!openURL) {
+        completionHandler();
+        return;
+    }
+
+    [UIApplication.sharedApplication openURL:openURL
+                                     options:[NSMutableDictionary dictionary]
+                           completionHandler:^(BOOL _) {
+        completionHandler();
+    }];
+}
+
+#pragma mark Static methods
+
++ (id)sharedInstance {
+   static ZBNotificationManager *instance = nil;
+   if (instance == nil) {
+       instance = [ZBNotificationManager new];
+       ZBDatabaseManager *databaseManager = [ZBDatabaseManager sharedInstance];
+       [databaseManager addDatabaseDelegate:instance];
+   }
+   return instance;
+}
+
+@end
