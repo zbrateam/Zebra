dfe7e74bb3c1639db4ed80c8fafd6663912e209d
Wilson Styres <wstyres@gmail.com>
2020-11-01 21:29:30 -0500

    [Packages] Starting over

--- Zebra/UI/Packages/ZBPackageListViewController.h
+++ Zebra/UI/Packages/ZBPackageListViewController.h
@@ -13,15 +13,12 @@
 NS_ASSUME_NONNULL_BEGIN
 
 @interface ZBPackageListViewController : UITableViewController
-@property (nonatomic, assign) BOOL batchLoad;
-@property (nonatomic, assign) BOOL isPerformingBatchLoad;
-@property (nonatomic, assign) BOOL continueBatchLoad;
-@property (nonatomic, assign) int batchLoadCount;
-@property (readwrite, copy, nonatomic) NSArray <NSArray *> *tableData;
+
+#pragma mark - Initializers
 - (instancetype)initWithSource:(ZBSource *)source;
 - (instancetype)initWithSource:(ZBSource *)source section:(NSString *_Nullable)section;
-- (void)refreshTable;
-- (NSArray *)contextMenuActionItemsForIndexPath:(NSIndexPath *)indexPath;
+
+
 @end
 
 NS_ASSUME_NONNULL_END
