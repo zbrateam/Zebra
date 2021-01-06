c01a955784e9b7d8cb3057ada85c28f78c5e1cbe
Andrew Abosh <andrewaboshartworks@gmail.com>
2021-01-05 22:59:21 -0500

    [Home] Create ZBFeaturedPackageCollectionViewCell

---
+++ Zebra/Tabs/Home/Views/ZBFeaturedPackageCollectionViewCell.h
@@ -0,0 +1,17 @@
+//
+//  ZBFeaturedPackageCollectionViewCell.h
+//  Zebra
+//
+//  Created by Andrew Abosh on 2021-01-05.
+//  Copyright © 2021 Wilson Styres. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ZBFeaturedPackageCollectionViewCell : UICollectionViewCell
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Zebra/Tabs/Home/Views/ZBFeaturedPackageCollectionViewCell.m
@@ -0,0 +1,18 @@
+//
+//  ZBFeaturedPackageCollectionViewCell.m
+//  Zebra
+//
+//  Created by Andrew Abosh on 2021-01-05.
+//  Copyright © 2021 Wilson Styres. All rights reserved.
+//
+
+#import "ZBFeaturedPackageCollectionViewCell.h"
+
+@implementation ZBFeaturedPackageCollectionViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+@end
