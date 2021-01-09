caeaf6482ad59b048aa650638421ebf43cabe799
Wilson Styres <wstyres@gmail.com>
2021-01-09 12:15:21 -0500

    [Sources] Text view purdification

--- Zebra/UI/Sources/ZBSourceBulkAddViewController.h
+++ Zebra/UI/Sources/ZBSourceBulkAddViewController.h
@@ -10,7 +10,7 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface ZBSourceBulkAddViewController : UIViewController
+@interface ZBSourceBulkAddViewController : UIViewController <UITextViewDelegate>
 
 @end
 
--- Zebra/UI/Sources/ZBSourceBulkAddViewController.m
+++ Zebra/UI/Sources/ZBSourceBulkAddViewController.m
@@ -10,9 +10,12 @@
 
 @interface ZBSourceBulkAddViewController ()
 @property UITextView *textView;
+@property NSLayoutConstraint *textViewBottomConstraint;
 @end
 
 @implementation ZBSourceBulkAddViewController
+
+#pragma mark - Initializers
 
 - (instancetype)init {
     self = [super init];
@@ -21,33 +24,71 @@
         self.title = NSLocalizedString(@"Bulk Add", @"");
         
         self.textView = [[UITextView alloc] init];
+        self.textView.delegate = self;
     }
     
     return self;
 }
 
+#pragma mark - View Controller Lifecycle
+
 - (void)loadView {
     [super loadView];
     
     self.view.backgroundColor = [UIColor groupTableViewBackgroundColor];
+    self.textViewBottomConstraint = [[_textView bottomAnchor] constraintEqualToAnchor:self.view.safeAreaLayoutGuide.bottomAnchor constant:-10];
     
     [self.view addSubview:_textView];
     [NSLayoutConstraint activateConstraints:@[
         [[_textView leadingAnchor] constraintEqualToAnchor:self.view.safeAreaLayoutGuide.leadingAnchor constant:10],
         [[_textView trailingAnchor] constraintEqualToAnchor:self.view.safeAreaLayoutGuide.trailingAnchor constant:-10],
         [[_textView topAnchor] constraintEqualToAnchor:self.view.safeAreaLayoutGuide.topAnchor constant:10],
-        [[_textView bottomAnchor] constraintEqualToAnchor:self.view.safeAreaLayoutGuide.bottomAnchor constant:10],
+        self.textViewBottomConstraint
     ]];
     _textView.translatesAutoresizingMaskIntoConstraints = NO;
     
     _textView.layer.cornerRadius = self.view.frame.size.height / 38.5;
     _textView.layer.masksToBounds = NO;
-    _textView.contentInset = UIEdgeInsetsMake(10, 20, 10, 10);
+    _textView.textContainerInset = UIEdgeInsetsMake(10, 20, 10, 10);
+    
+    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:NSLocalizedString(@"Verify", @"") style:UIBarButtonItemStyleDone target:self action:@selector(verifySources)];
+    self.navigationItem.rightBarButtonItem.enabled = NO;
 }
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view.
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillShow:) name:UIKeyboardWillShowNotification object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillHide:) name:UIKeyboardWillHideNotification object:nil];
+}
+
+- (void)viewDidDisappear:(BOOL)animated {
+    [super viewDidDisappear:animated];
+    
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
+}
+
+- (void)verifySources {
+    
+}
+
+#pragma mark - Keyboard Notifications
+
+- (void)keyboardWillShow:(NSNotification *)notification {
+    CGRect keyboardFrame = ((NSValue *)notification.userInfo[UIKeyboardFrameEndUserInfoKey]).CGRectValue;
+    self.textViewBottomConstraint.constant = -keyboardFrame.size.height;
+    self.textViewBottomConstraint.active = YES;
+}
+
+- (void)keyboardWillHide:(NSNotification *)notification {
+    self.textViewBottomConstraint.constant = 10;
+    self.textViewBottomConstraint.active = YES;
+}
+
+#pragma mark - Text View Delegate
+
+- (void)textViewDidChange:(UITextView *)textView {
+    self.navigationItem.rightBarButtonItem.enabled = ![[textView.textStorage.string stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]] isEqualToString:@""];
 }
 
 @end
