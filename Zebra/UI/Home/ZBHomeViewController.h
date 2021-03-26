46f64f2317560f6b1875ef4f65ee1e634c00ff60
Wilson Styres <wstyres@gmail.com>
2021-03-25 20:12:13 -0400

    Redo home because I'm bored

--- Zebra/UI/Home/ZBHomeViewController.h
+++ Zebra/UI/Home/ZBHomeViewController.h
@@ -6,27 +6,8 @@
 //  Copyright © 2019 Wilson Styres. All rights reserved.
 //
 
-#import <Extensions/UIColor+GlobalColors.h>
-#import <ZBDevice.h>
-#import <Tabs/Home/Settings/ZBMainSettingsTableViewController.h>
-#import <Model/ZBSource.h>
-#import "ZBFeaturedCollectionViewCell.h"
-#import <Extensions/UIImage+UIKitImage.h>
+#import <UIKit/UIKit.h>
 
-@import SDWebImage;
-@import SafariServices;
-@import UIKit;
+@interface ZBHomeViewController : UITableViewController
 
-@interface ZBHomeViewController : UITableViewController <UICollectionViewDelegate, UICollectionViewDataSource, UIAdaptivePresentationControllerDelegate, SFSafariViewControllerDelegate>
-@property (weak, nonatomic) IBOutlet UIBarButtonItem *darkModeButton;
-@property (weak, nonatomic) IBOutlet UIBarButtonItem *settingsButton;
-@property (weak, nonatomic) IBOutlet UIView *footerView;
-@property (weak, nonatomic) IBOutlet UIView *headerView;
-@property (weak, nonatomic) IBOutlet UILabel *footerLabel;
-@property (weak, nonatomic) IBOutlet UILabel *udidLabel;
-@property (weak, nonatomic) IBOutlet UICollectionView *featuredCollection;
-@property NSMutableArray *allFeatured;
-@property NSMutableArray *selectedFeatured;
-@property NSInteger cellNumber;
-@property NSUserDefaults *defaults;
 @end
