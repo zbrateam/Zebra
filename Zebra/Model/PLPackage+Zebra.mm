c6b2c23e7dee14b1a2a7245adfa228c1e27b6516
Wilson Styres <wstyres@gmail.com>
2021-04-04 22:31:53 -0400

    Add some more things back into information

--- Zebra/Model/PLPackage+Zebra.mm
+++ Zebra/Model/PLPackage+Zebra.mm
@@ -60,7 +60,7 @@
     }
     
     if (installed) {
-        NSString *installedSize = [self installedSizeString];
+        NSString *installedSize = self.installedSizeString;
         if (installedSize) { // Show the installed size
             NSMutableDictionary *installedSizeInfo = [@{@"name": NSLocalizedString(@"Size", @""), @"value": installedSize, @"cellType": @"info", @"class": @"ZBInstalledFilesTableViewController"} mutableCopy];
             [information addObject:installedSizeInfo];
@@ -70,36 +70,36 @@
             [information addObject:installedFilesInfo];
         }
     }
-//    else { // Show the download size
-//        NSString *downloadSize = [self downloadSizeString];
-//        if (downloadSize) {
-//            NSMutableDictionary *downloadSizeInfo = [@{@"name": NSLocalizedString(@"Size", @""), @"value": downloadSize, @"cellType": @"info"} mutableCopy];
-//            [information addObject:downloadSizeInfo];
-//        }
-//    }
+    else { // Show the download size
+        NSString *downloadSize = self.downloadSizeString;
+        if (downloadSize) {
+            NSMutableDictionary *downloadSizeInfo = [@{@"name": NSLocalizedString(@"Size", @""), @"value": downloadSize, @"cellType": @"info"} mutableCopy];
+            [information addObject:downloadSizeInfo];
+        }
+    }
     
-//    NSString *authorName = [self authorName];
-//    if (authorName) {
-//        NSDictionary *authorNameInfo = @{@"name": NSLocalizedString(@"Author", @""), @"value": authorName, @"cellType": @"info", @"class": @"ZBPackagesByAuthorTableViewController"};
-//        [information addObject:authorNameInfo];
-//    }
-//    else {
-//        NSString *maintainerName = [self maintainerName];
-//        if (maintainerName) {
-//            NSDictionary *maintainerNameInfo = @{@"name": NSLocalizedString(@"Maintainer", @""), @"value": maintainerName, @"cellType": @"info"};
-//            [information addObject:maintainerNameInfo];
-//        }
-//    }
+    NSString *authorName = [self authorName];
+    if (authorName) {
+        NSDictionary *authorNameInfo = @{@"name": NSLocalizedString(@"Author", @""), @"value": authorName, @"cellType": @"info", @"class": @"ZBPackagesByAuthorTableViewController"};
+        [information addObject:authorNameInfo];
+    }
+    else {
+        NSString *maintainerName = [self maintainerName];
+        if (maintainerName) {
+            NSDictionary *maintainerNameInfo = @{@"name": NSLocalizedString(@"Maintainer", @""), @"value": maintainerName, @"cellType": @"info"};
+            [information addObject:maintainerNameInfo];
+        }
+    }
     
     NSString *sourceOrigin = [[self source] origin];
     if (sourceOrigin) {
-//        if (self.source.remote) {
-//            NSDictionary *sourceOriginInfo = @{@"name": NSLocalizedString(@"Source", @""), @"value": sourceOrigin, @"cellType": @"info", @"class": @"ZBSourceViewController"};
-//            [information addObject:sourceOriginInfo];
-//        } else {
+        if (self.source.remote) {
+            NSDictionary *sourceOriginInfo = @{@"name": NSLocalizedString(@"Source", @""), @"value": sourceOrigin, @"cellType": @"info", @"class": @"ZBSourceViewController"};
+            [information addObject:sourceOriginInfo];
+        } else {
             NSDictionary *sourceOriginInfo = @{@"name": NSLocalizedString(@"Source", @""), @"value": sourceOrigin, @"cellType": @"info"};
             [information addObject:sourceOriginInfo];
-//        }
+        }
     }
     
     NSString *section = [self section];
@@ -108,46 +108,46 @@
         [information addObject:sectionInfo];
     }
     
-//    if (self.depends.count) {
-//        NSMutableArray *strippedDepends = [NSMutableArray new];
-//        for (NSString *depend in self.depends) {
-//            if ([depend containsString:@" | "]) {
-//                NSArray *ord = [depend componentsSeparatedByString:@" | "];
-//                [strippedDepends addObject:[ord componentsJoinedByString:@" or "]];
-//            }
-//            else if (![strippedDepends containsObject:depend]) {
-//                [strippedDepends addObject:depend];
-//            }
-//        }
-//
-//        NSDictionary *dependsInfo = @{@"name": NSLocalizedString(@"Dependencies", @""), @"value": [NSString stringWithFormat:@"%lu Dependencies", (unsigned long)strippedDepends.count], @"cellType": @"info", @"more": [strippedDepends componentsJoinedByString:@"\n"]};
-//        [information addObject:dependsInfo];
-//    }
-//
-//    if (self.conflicts.count) {
-//        NSMutableArray *strippedConflicts = [NSMutableArray new];
-//        for (NSString *conflict in self.conflicts) {
-//            if ([conflict containsString:@" | "]) {
-//                NSArray *orc = [conflict componentsSeparatedByString:@" | "];
-//                for (__strong NSString *conflict in orc) {
-//                    NSRange range = [conflict rangeOfString:@"("];
-//                    if (range.location != NSNotFound) {
-//                        conflict = [conflict substringToIndex:range.location];
-//                    }
-//
-//                    if (![strippedConflicts containsObject:conflict]) {
-//                        [strippedConflicts addObject:conflict];
-//                    }
-//                }
-//            }
-//            else if (![strippedConflicts containsObject:conflict]) {
-//                [strippedConflicts addObject:conflict];
-//            }
-//        }
-//
-//        NSDictionary *conflictsInfo = @{@"name": NSLocalizedString(@"Conflicts", @""), @"value": [NSString stringWithFormat:@"%lu Conflicts", (unsigned long)strippedConflicts.count], @"cellType": @"info", @"more": [strippedConflicts componentsJoinedByString:@"\n"]};
-//        [information addObject:conflictsInfo];
-//    }
+    if (self.depends.count) {
+        NSMutableArray *strippedDepends = [NSMutableArray new];
+        for (NSString *depend in self.depends) {
+            if ([depend containsString:@" | "]) {
+                NSArray *ord = [depend componentsSeparatedByString:@" | "];
+                [strippedDepends addObject:[ord componentsJoinedByString:@" or "]];
+            }
+            else if (![strippedDepends containsObject:depend]) {
+                [strippedDepends addObject:depend];
+            }
+        }
+
+        NSDictionary *dependsInfo = @{@"name": NSLocalizedString(@"Dependencies", @""), @"value": [NSString stringWithFormat:@"%lu Dependencies", (unsigned long)strippedDepends.count], @"cellType": @"info", @"more": [strippedDepends componentsJoinedByString:@"\n"]};
+        [information addObject:dependsInfo];
+    }
+
+    if (self.conflicts.count) {
+        NSMutableArray *strippedConflicts = [NSMutableArray new];
+        for (NSString *conflict in self.conflicts) {
+            if ([conflict containsString:@" | "]) {
+                NSArray *orc = [conflict componentsSeparatedByString:@" | "];
+                for (__strong NSString *conflict in orc) {
+                    NSRange range = [conflict rangeOfString:@"("];
+                    if (range.location != NSNotFound) {
+                        conflict = [conflict substringToIndex:range.location];
+                    }
+
+                    if (![strippedConflicts containsObject:conflict]) {
+                        [strippedConflicts addObject:conflict];
+                    }
+                }
+            }
+            else if (![strippedConflicts containsObject:conflict]) {
+                [strippedConflicts addObject:conflict];
+            }
+        }
+
+        NSDictionary *conflictsInfo = @{@"name": NSLocalizedString(@"Conflicts", @""), @"value": [NSString stringWithFormat:@"%lu Conflicts", (unsigned long)strippedConflicts.count], @"cellType": @"info", @"more": [strippedConflicts componentsJoinedByString:@"\n"]};
+        [information addObject:conflictsInfo];
+    }
 //
 //    if (self.lowestCompatibleVersion) {
 //        NSString *compatibility;
@@ -167,17 +167,17 @@
 //        [information addObject:homepageInfo];
 //    }
 //
-//    BOOL showSupport = [self authorEmail] || [self maintainerEmail];
-//    if (showSupport) {
-//        NSDictionary *homepageInfo = @{@"name": NSLocalizedString(@"Support", @""), @"cellType": @"link", @"class": @"ZBPackageSupportViewController", @"image": @"Email"};
-//        [information addObject:homepageInfo];
-//    }
-//
-//    NSURL *depiction = [self depictionURL];
-//    if (depiction) {
-//        NSDictionary *depictionInfo = @{@"name": NSLocalizedString(@"View Depiction in Safari", @""), @"cellType": @"link", @"link": depiction, @"image": @"Web Link"};
-//        [information addObject:depictionInfo];
-//    }
+    BOOL showSupport = self.authorEmail || self.maintainerEmail;
+    if (showSupport) {
+        NSDictionary *homepageInfo = @{@"name": NSLocalizedString(@"Support", @""), @"cellType": @"link", @"class": @"ZBPackageSupportViewController", @"image": @"Email"};
+        [information addObject:homepageInfo];
+    }
+
+    NSURL *depiction = self.depictionURL;
+    if (depiction) {
+        NSDictionary *depictionInfo = @{@"name": NSLocalizedString(@"View Depiction in Safari", @""), @"cellType": @"link", @"link": depiction, @"image": @"Web Link"};
+        [information addObject:depictionInfo];
+    }
     
     return information;
 }
