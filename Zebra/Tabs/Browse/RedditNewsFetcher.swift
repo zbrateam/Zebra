756b59be727481f5e7314d0e5d100123a3b87557
Adam Demasi <adam@hbang.com.au>
2022-04-27 18:19:15 +0930

    [Browse] Update to new reddit news url and tag format

--- Zebra/Tabs/Browse/RedditNewsFetcher.swift
+++ Zebra/Tabs/Browse/RedditNewsFetcher.swift
@@ -16,21 +16,29 @@ fileprivate struct RedditNewsItem: Codable {
 	let title: String
 	let url: String
 	let thumbnail: String?
-	let tag: String?
+	let tags: RedditNewsTag?
 }
 
-fileprivate enum RedditNewsTag: String {
+fileprivate enum RedditNewsTag: String, Codable {
 	case news = "news"
 	case release = "release"
+	case releaseFree = "release,free"
+	case releasePaid = "release,paid"
 	case update = "update"
+	case updateFree = "update,free"
+	case updatePaid = "update,paid"
 	case upcoming = "upcoming"
 
 	var text: String {
 		switch self {
-		case .news:     return .localize("News")
-		case .release:  return .localize("Release")
-		case .update:   return .localize("Update")
-		case .upcoming: return .localize("Upcoming")
+		case .news:        return .localize("News")
+		case .release:     return .localize("Release")
+		case .releaseFree: return .localize("Free Release")
+		case .releasePaid: return .localize("Paid Release")
+		case .update:      return .localize("Update")
+		case .updateFree:  return .localize("Free Update")
+		case .updatePaid:  return .localize("Paid Update")
+		case .upcoming:    return .localize("Upcoming")
 		}
 	}
 }
@@ -59,12 +67,12 @@ class RedditNewsFetcher {
 	}
 
 	static func fetch() async throws -> [CarouselItem] {
-		let request = URLRequest(url: URL(string: "https://zbrateam.github.io/reddit-news/reddit-news-relevance.json")!)
+		let request = URLRequest(url: URL(string: "https://zbrateam.github.io/api/reddit-news-relevance.json")!)
 
 		let json: RedditNewsRoot = try await HTTPRequest.json(for: request)
 		let items = json.data.compactMap { item in
-			CarouselItem(title: item.title,
-									 subtitle: (RedditNewsTag(rawValue: item.tag ?? "") ?? .news).text,
+			CarouselItem(title: item.title.redditAPIUnescaped,
+									 subtitle: (item.tags ?? .news).text,
 									 url: URL(string: item.url.redditAPIUnescaped)!,
 									 imageURL: URL(string: item.thumbnail?.redditAPIUnescaped ?? ""))
 		}
