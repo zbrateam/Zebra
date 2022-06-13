58bb6f2a71d59b2e93c6c6d84a15b8505ac84e52
Adam Demasi <adam@hbang.com.au>
2022-06-13 19:04:15 +0930

    [Extensions] Can swift calm down with the deprecations for 5 minutes

--- Zebra/Extensions/TaskPriority+Additions.swift
+++ Zebra/Extensions/TaskPriority+Additions.swift
@@ -9,15 +9,25 @@
 import Foundation
 
 extension TaskPriority {
+	// It‘s deprecated, but still seems to be used by the system in some instances. Not checking for
+	// this can cause a runtime warning about QoS .userInteractive depending on QoS .default, which is
+	// a potential deadlock. Since TaskPriority.init(rawValue:) is public, should be safe to do this.
+	private static let deprecatedUserInteractive = TaskPriority(rawValue: 33)
+
 	var qos: DispatchQoS {
 		switch self {
-		case .high:          return .userInitiated
-		case .medium:        return .default
-		case .low:           return .utility
-		case .utility:       return .utility
-		case .background:    return .background
-		case .userInitiated: return .userInitiated
-		default:             return .default
+		case .medium:
+			return .default
+		case .low, .utility:
+			return .utility
+		case .background:
+			return .background
+		case .high, .userInitiated:
+			return .userInitiated
+		case .deprecatedUserInteractive:
+			return .userInteractive
+		default:
+			return .default
 		}
 	}
 }
