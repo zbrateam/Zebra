bf36f2242349ab66ab03d8f3b8dbe3ffda960dde
Adam Demasi <adam@hbang.com.au>
2022-12-14 00:55:24 +1030

    [Settings] Fix app icon selector on rootless (no loose 60x60 pngs)

--- Zebra/Tabs/Home/Settings/App Icon/ZBAlternateIconCell.m
+++ Zebra/Tabs/Home/Settings/App Icon/ZBAlternateIconCell.m
@@ -60,7 +60,14 @@
         // Nil selected icon means the default icon is currently active.
         BOOL isSelected = [item[@"iconName"] isEqualToString:selectedIconName ?: @"AppIcon"];
         BOOL border = [item[@"border"] boolValue];
+#if __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_15_0
+        NSString *iconName = item[@"iconName"];
+        if ([iconName isEqualToString:@"AppIcon"]) {
+            iconName = @"AppIcon60x60";
+        }
+#else
         NSString *iconName = [item[@"iconName"] stringByAppendingString:@"60x60"];
+#endif
         UIImage *image = [[UIImage imageNamed:iconName] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
 
         UIButton *button = [UIButton buttonWithType:UIButtonTypeSystem];
